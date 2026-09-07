d5b786fbfdd23872dfcbfc586372527c5ad580da
VedikaMamidwar <vedikamamidwar5@gmail.com>
2026-09-07 22:32:56 +0530

    new file added

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -15,7 +15,7 @@ import ResetPassword from "../pages/auth/ResetPassword";
 import CompleteProfile from "../pages/dashboard/CompleteProfile";
 import Dashboard from "../pages/dashboard/Dashboard";
 import BuildTeam from "../pages/dashboard/BuildTeam";
-import Hackathon from "../pages/dashboard/Hackathon";
+
 import FindTeammates from "../pages/dashboard/FindTeammates";
 
 // ================= USER =================
@@ -127,7 +127,6 @@ export default function AppRoutes() {
 
             {/* ================= HACKATHONS ================= */}
 
-            <Route path="/hackathons" element={<Hackathon />} />
 
 
             {/* ================= USER ================= */}
